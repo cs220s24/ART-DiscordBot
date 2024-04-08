4733927abad7b3a01478358bb65280257e958951
Marena <abboudm@moravian.edu>
2024-04-08 12:04:25 -0400

    updated code

--- bot.py
+++ bot.py
@@ -24,6 +24,36 @@ async def send_message(message: Message, user_message: str) -> None:
 
     try:
         response: str = get_response(user_message)
-        await message.author.send(response) if is_private else message.channel.send(response)
+        await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+
+# Handeling the startup for the bot
+        
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+
+#Handeling incoming messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+
+
+    # Main entry point
+    def main() -> None:
+        client.run(token=TOKEN)
+
+    
+    if __name__ == '__main__':
+        main()
