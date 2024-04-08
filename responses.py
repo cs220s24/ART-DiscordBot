585796c0dc4569f208add01725a488194fbd8560
TeranGmoravian.edu <TeranG@moravian.edu>
2024-04-08 11:51:02 -0400

    code in reponses.py

--- responses.py
+++ responses.py
@@ -1,1 +1,9 @@
-print("hello")
+from random import choice, randint
+
+def get_response(user_input: str) -> str:
+	lowered: str = user_input.lower
+
+	if lowered == '':
+		return 'Say something!'
+	elif 'hello' in lowered:
+		return 'hello there' 
