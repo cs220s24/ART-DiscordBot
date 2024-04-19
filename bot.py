d43770d8751f7b3f62382807b48f8f7cba6c101c
Marena <abboudm@moravian.edu>
2024-04-18 21:36:03 -0400

    added redis database and URLs

--- bot.py
+++ bot.py
@@ -1,35 +1,66 @@
-# bot.py
 import os
 import random
+import redis
+from discord import Intents, Client
 
-from discord import Intents, Client, Message
+# Import dotenv if you're using environment variables
 from dotenv import load_dotenv
 
-intents: Intents = Intents.default()
-intents.message_content = True
-client: Client = Client(intents=intents)
-
-
+# Load environment variables from .env file
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
+
+# Connect to Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# Sample cat and dog image URLs from Imgur
+cat_image_urls = [
+    'https://imgur.com/SpCbHBI',
+    'https://imgur.com/b9T47yP',
+    'https://imgur.com/8dF73nu'
+]
+
+dog_image_urls = [
+    'https://imgur.com/sjpa0Gg',
+    'https://imgur.com/Cpr6QEu',
+    'https://imgur.com/Dg5tv0L'
+]
+
+# Function to initialize the Redis database with cat and dog image URLs
+def initialize_database():
+    for image_url in cat_image_urls:
+        redis_client.sadd('cats', image_url)
+    for image_url in dog_image_urls:
+        redis_client.sadd('dogs', image_url)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    # Initialize the database when the bot is ready
+    initialize_database()
+    print('Database initialized with cat and dog image URLs.')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    dogImages = ['dog1','dog2','dog3' ]
-    catImages = ['cat1','cat2','cat3' ]
-
     if message.content == 'Cats':
-        response = random.choice(catImages)
-        await message.channel.send(response)
-    if message.content == 'Dogs':
-        response = random.choice(dogImages)
-        await message.channel.send(response)
+        image_url = redis_client.srandmember('cats')
+        if image_url:
+            await message.channel.send(image_url.decode('utf-8'))
+        else:
+            await message.channel.send("No cat images available.")
+    elif message.content == 'Dogs':
+        image_url = redis_client.srandmember('dogs')
+        if image_url:
+            await message.channel.send(image_url.decode('utf-8'))
+        else:
+            await message.channel.send("No dog images available.")
 
+# Retrieve the bot token from environment variables
+TOKEN = os.getenv('DISCORD_TOKEN')
 client.run(TOKEN)
