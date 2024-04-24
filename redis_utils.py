f528bafdf25804e7250ef273507701e74643eed6
Marena <abboudm@moravian.edu>
2024-04-24 16:01:44 -0400

    new file that contains the database

---
+++ redis_utils.py
@@ -0,0 +1,26 @@
+import redis
+
+# Sample cat and dog image URLs from Imgur
+cat_image_urls = [
+    'https://imgur.com/SpCbHBI',
+    'https://imgur.com/b9T47yP',
+    'https://imgur.com/8dF73nu'
+]
+
+dog_image_urls = [
+    'https://imgur.com/sjpa0Gg',
+    'https://imgur.com/Cpr6QEu',
+    'https://imgur.com/Dg5tv0L'
+]
+
+# Function to initialize the Redis database with cat and dog image URLs
+def initialize_database():
+    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    # Clear existing data
+    redis_client.flushdb()
+    # Add new URLs
+    for image_url in cat_image_urls:
+        redis_client.sadd('cats', image_url)
+    for image_url in dog_image_urls:
+        redis_client.sadd('dogs', image_url)
+
